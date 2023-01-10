cdef97e64d29976b5cb63939842e37673abf1e56
Valdecir Rml <valdecir.rml@gmail.com>
2023-01-10 13:45:20 -0300

    Criação Mapa. Adição da figura,  tema e estilo do mapa a ser usado

--- dashboard.py
+++ dashboard.py
@@ -23,18 +23,58 @@ import pandas as pd
 # modulo do python usado para fazer leitura  e manipulação de dados json
 import json
 
-#---------------------------------------------------------------------------------------------------
-#Estudo e Manipulação de dados, filtros, extrações do arquivo csv, tipos de dados, 
-#conhecimento das informações que serão usadas para os gráficos no dashboard 
-# (manipula_dados.py)
-
-#import manipula_dados
-
 #importo dados especificos dos estados
 df_states = pd.read_csv("df_states.csv")
 #importo dados especificos dos Brasil
 df_brasil = pd.read_csv("df_brasil.csv")
 
-#importando arquivo json da localização dos estados do brasil utilizando somente o método de leitura opcao 'r' 
-#(importa_json.py)
-import importa_json
+#importando arquivo json da localizacao dos estados do brasil utilizando somente o método de leitura opcao 'r'
+brazil_states = json.load(open("geojson/brazil_geo.json", "r"))
+
+#-------------------------------------------------------------------------------------------------------------------
+#Criação Mapa
+#instancia da classe onde vai conter o dashboard, dash modulo instancia a classe 'Dash'
+
+
+# __name__, parametro basico
+# external_stylesheets, para utilizar uma folha de estilo externa
+# dbc dash bootstrap componentes, componentes para leiaute
+#themes estiliza com temas mais bonitos
+# CYBORG tipo de tema escuro
+#nos temas podemos usar streams que estão localizadas no endereço www.bootshwatch.comm para ver estilos que podemos usar
+# ou na propria bibliteca do bootstrap 'https://dash-bootstrap-components.opensource.faculty.ai/', parte documentação acessa opcao Themes
+# a folha de estilo feita externa faz algumas alteracoes no tema CYBORG para ficar mais bonito
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.CYBORG])
+
+#filtro um dia a priore, por enqto fixo
+df_states_ = df_states[df_states["data"] == "2020-05-13"]
+
+# teste listar filtro do dia priore 
+print(df_states_)
+
+#-----------------------------------------------------------------------
+#figure elemento que vai conter o mapa
+
+#choropleth_mapbox, mapas onde tem as divisoes coloridos para especificar informações (informações relacao ao covid)
+#mapbox opcao para criar graficos visualmente mais bonitos
+#df_states, passo primeiro parametro o data frame que possuem os dados q vou usar
+#segundo parametro com a coluna onde tera o 'ID' que vai casar com o campo do mapa (geojson)
+#terceiro parametros pinta a coluna cfe a coluna de casosNovos
+#quarto parametro passo o arquivo geojson que ele vai fazer a referencia
+#quinto parametro passo uma paleta de cores 
+#sexto parametro passo a opacidade 
+#e por ultimo aplico o hover_data que é um dicionario com informações que quero q apresente
+#quando eu colocar o cursor em cima do estado, True ou False é se eu quero ou não mostrar
+#parametro center é onde eu quero deixar centrado inicialmente o mapa
+
+fig = px.choropleth_mapbox(df_states_, locations="estado", color="casosNovos", 
+                           center={"lat": -16.95, "lon": -47.78}, 
+                           geojson=brazil_states, color_continuous_scale="Redor", opacity=0.4,
+                           hover_data={"casosAcumulado": True, "casosNovos": True, "obitosNovos": True, "estado": True})
+
+#================================================                           
+# Criando Estilo do layout da figura
+fig.update_layout(
+    mapbox_style="carto-darkmatter"
+)
+
--- importa_json.py
+++ importa_json.py
@@ -1,34 +1,4 @@
 # modulo do python usado para fazer leitura  e manipulação de dados json
 import json
-
 #importando arquivo json da localizacao dos estados do brasil utilizando somente o método de leitura opcao 'r'
 brazil_states = json.load(open("geojson/brazil_geo.json", "r"))
-
-#verificando type da dados
-print(type(brazil_states))
-
-#se for dict (dicionario), verificar quais as chaves possui
-print((brazil_states.keys()))
-
-#verifico o que tem dentro das chaves
-print((brazil_states["type"]))
-print((brazil_states["features"]))
-
-#verifico o que o tipo da chave features
-print((type(brazil_states["features"])))
-
-#verifico type da chave features - List
-print((type(brazil_states["features"])))
-
-#Como é uma lista verifico primeiro elemento da chave 'features'
-print((brazil_states["features"][0]))
-
-#verifico o tipo de elemento que é 'dict'
-print((type(brazil_states["features"][0])))
-#identifico que tenho uma lista de dicionarios ...
-#pergunto quais são as chaves desses elementos q sao dicionarios
-print((brazil_states["features"][0].keys()))
-
-#indetifico campo principal da lista
-print((brazil_states["features"][0]["id"]))
-print((brazil_states["features"][0]["geometry"]))
